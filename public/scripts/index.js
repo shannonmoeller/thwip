7c6ef7d76de5a07791bd04a7183bed751b304b4e
Shannon Moeller <me@shannonmoeller.com>
2022-02-09 06:15:51 +0000

    color, speed, and gravity

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -6,7 +6,9 @@ let canvas = document.querySelector('canvas');
 let ctx = createContext(canvas);
 
 let timeout = null;
-let gravity = 0.08;
+let gravity = 0.04;
+let jump = 1;
+let pump = 0.02125;
 let state = createGameState();
 let ropeLength = getDistance(state.player, state.anchor);
 
@@ -16,9 +18,16 @@ function createGameState() {
 		isGrabbing: false,
 
 		anchor: {
-			x: 150,
+			x: 200,
 			y: 0,
 			mass: 0,
+		},
+
+		hold: {
+			x: 100,
+			x0: 100,
+			y: 100,
+			y0: 100,
 		},
 
 		player: {
@@ -26,21 +35,21 @@ function createGameState() {
 			x0: 100,
 			y: 100,
 			y0: 100,
-			w: 10,
+			w: 20,
 			h: 20,
 			mass: 1,
 		},
 
 		platformA: {
-			x: 100,
-			y: 120,
+			x: 80,
+			y: 110,
 			w: 60,
 		},
 
 		platformB: {
-			x: 300 + Math.round(Math.random() * 150),
+			x: 300 + Math.round(Math.random() * 300),
 			y: 200 + Math.round(Math.random() * 100),
-			w: 20 + Math.round(Math.random() * 150),
+			w: 20 + Math.round(Math.random() * 200),
 		},
 
 		reset() {
@@ -73,7 +82,7 @@ function updateDude() {
 	state.player.y0 = y;
 
 	if (state.isGrabbing) {
-		state.player.x0 -= 0.04;
+		state.player.x0 -= pump;
 
 		for (let i = 3; i--; ) {
 			constrain(state.player, state.anchor, { length: ropeLength });
@@ -85,7 +94,7 @@ function updateDude() {
 		return;
 	}
 
-	if (state.player.y + state.player.h < state.platformB.y) {
+	if (state.player.y + state.player.h * 0.5 < state.platformB.y) {
 		return;
 	}
 
@@ -103,8 +112,8 @@ function updateDude() {
 		return;
 	}
 
-	if (state.player.y + state.player.h * 0.5 < state.platformB.y) {
-		state.player.y = state.platformB.y - state.player.h;
+	if (state.player.y + state.player.h * 0.1 < state.platformB.y) {
+		state.player.y = state.platformB.y - state.player.h * 0.5;
 	}
 
 	state.isSwinging = false;
@@ -136,17 +145,14 @@ function renderPlatform(platform) {
 
 	ctx.translate(x, y);
 
-	ctx.fillStyle = 'black';
+	ctx.fillStyle = 'white';
 	ctx.fillRect(w * -0.5, 0, w, ctx.height * 2);
 
-	ctx.fillStyle = 'dodgerblue';
+	ctx.fillStyle = 'deeppink';
 	ctx.fillRect(w * -0.5, 0, w, 5);
 
-	ctx.fillStyle = 'deeppink';
-	ctx.fillRect(-15, 0, 30, 5);
-
 	ctx.fillStyle = 'yellow';
-	ctx.fillRect(-5, 0, 10, 5);
+	ctx.fillRect(-10, 0, 20, 5);
 
 	ctx.restore();
 }
@@ -161,6 +167,8 @@ function renderRope() {
 	ctx.beginPath();
 	ctx.moveTo(state.anchor.x, state.anchor.y);
 	ctx.lineTo(state.player.x, state.player.y);
+
+	ctx.strokeStyle = 'dodgerblue';
 	ctx.stroke();
 
 	ctx.restore();
@@ -170,15 +178,11 @@ function renderPlayer() {
 	ctx.save();
 
 	let { x, y, h, w } = state.player;
-	let head = 10;
 
-	ctx.translate(x, y - head);
+	ctx.translate(x, y);
 
-	ctx.fillStyle = 'white';
-	ctx.fillRect(w * -0.5, 0, w, h + head);
-
-	ctx.strokeStyle = 'black';
-	ctx.strokeRect(w * -0.5, 0, w, h + head);
+	ctx.fillStyle = 'dodgerblue';
+	ctx.fillRect(w * -0.5, h * -0.5, w, h);
 
 	ctx.restore();
 }
@@ -186,13 +190,17 @@ function renderPlayer() {
 resizeContext(ctx);
 addEventListener('resize', () => resizeContext(ctx), { passive: true });
 
+canvas.addEventListener('contextmenu', (event) => {
+	event.preventDefault();
+});
+
 canvas.addEventListener('pointerdown', () => {
 	clearTimeout(timeout);
 	state.reset();
 	state.isSwinging = true;
 	state.isGrabbing = true;
-	state.player.x0 = state.player.x + 2;
-	state.player.y0 = state.player.y + 1;
+	state.player.x0 = state.player.x + jump;
+	state.player.y0 = state.player.y + jump;
 });
 
 canvas.addEventListener('pointerup', () => {
